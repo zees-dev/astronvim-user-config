d500847c438cb4f7fffd044cc979bc2c6b1afddc
= <dev.786zshan@gmail.com>
2023-06-25 15:34:42 +1200

    updated config

--- options.lua
+++ options.lua
@@ -2,11 +2,12 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    wrap = true, -- sets vim.opt.wrap
+    tabstop = 2,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
