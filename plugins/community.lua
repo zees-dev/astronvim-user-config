a8e4736a50bdb40fd60181ef1a25f5b660039af8
= <dev.786zshan@gmail.com>
2023-07-02 00:57:04 +1200

    added common lsp server community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.rust" },
 }
