17565fa3ddc39c1a7920ed43527f61fa6252d32b
zees-dev <dev.786zshan@gmail.com>
2023-07-24 11:18:27 +1200

    copilot setup

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,21 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python" },
