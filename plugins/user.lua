5466d6ef4269e32e3fae073554d8fc2a27f11524
zees-dev <dev.786zshan@gmail.com>
2023-08-14 19:09:46 +1200

    default vscode theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,8 +9,6 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  "MunifTanjim/nui.nvim",
-  "nvim-lua/plenary.nvim",
   {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
@@ -30,36 +28,45 @@ return {
       "nvim-telescope/telescope.nvim",
     },
   },
-  {
-    "lewis6991/hover.nvim",
-    config = function()
-      require("hover").setup {
-        init = function()
-          -- Require providers
-          require "hover.providers.lsp"
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
-          -- require('hover.providers.jira')
-          -- require('hover.providers.man')
-          -- require('hover.providers.dictionary')
-        end,
-        preview_opts = {
-          border = nil,
-        },
-        -- Whether the contents of a currently open hover window should be moved
-        -- to a :h preview-window when pressing the hover keymap.
-        preview_window = false,
-        title = true,
-      }
-
-      -- Setup keymaps
-      vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
-      vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-    end,
-  },
+  -- {
+  --   "lewis6991/hover.nvim",
+  --   config = function()
+  --     require("hover").setup {
+  --       init = function()
+  --         -- Require providers
+  --         require "hover.providers.lsp"
+  --         -- require('hover.providers.gh')
+  --         -- require('hover.providers.gh_user')
+  --         -- require('hover.providers.jira')
+  --         -- require('hover.providers.man')
+  --         -- require('hover.providers.dictionary')
+  --       end,
+  --       preview_opts = {
+  --         border = nil,
+  --       },
+  --       -- Whether the contents of a currently open hover window should be moved
+  --       -- to a :h preview-window when pressing the hover keymap.
+  --       preview_window = false,
+  --       title = true,
+  --     }
+  --
+  --     -- Setup keymaps
+  --     vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+  --     vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+  --   end,
+  -- },
   {
     "f-person/git-blame.nvim",
     event = "VeryLazy",
   },
+
   "Mofiqul/vscode.nvim",
+  {
+    "ggandor/leap.nvim",
+    config = function() require("leap").add_default_mappings() end,
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    lazy = false,
+  },
 }
